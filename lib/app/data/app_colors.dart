a126c7b6cbbc0e30946c6c911c18273397c91a72
md-miyam <mdmiyam20@gmail.com>
2025-11-24 18:05:35 +0600

    need to merge with dev for font

--- lib/app/data/app_colors.dart
+++ lib/app/data/app_colors.dart
@@ -7,5 +7,9 @@ class AppColors{
   static const Color hintTextColor = Color(0xff606060);
   static const Color appBarBack = Color(0xffF8F8F8);
   static const Color appBarSub = Color(0xff595959);
+  static const Color appBarCircleAvatar = Color(0xfffffbed);
+  static const Color appBarCircleAvatarColor = Color(0xffFFD160);
+  static const Color homeStack = Color(0xffEDEBE4);
+
 }
 
--- lib/app/data/image_path.dart
+++ lib/app/data/image_path.dart
@@ -2,4 +2,6 @@ class ImagePath {
   static const String baseUrl = 'assets/images';
   static const String appLogo = '$baseUrl/app_logo.png';
   static const String arrowBack = '$baseUrl/arrow_back.png';
+  static const String profile = '$baseUrl/profile.png';
+  static const String notification = '$baseUrl/notification.png';
 }
