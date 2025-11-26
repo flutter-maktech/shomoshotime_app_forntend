c0797424648096d83609d965a261e69542fdab9f
Sashisaha <sashisaha98@gmail.com>
2025-11-26 12:21:53 +0600

    edit profile screen added

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:shomoshotime/app/data/app_colors.dart';
 import 'package:shomoshotime/app/data/app_text_styles.dart';
 import 'package:shomoshotime/app/data/image_path.dart';
+import 'package:shomoshotime/app/modules/common_widgets/custom_app_bar.dart';
 import 'package:shomoshotime/app/modules/common_widgets/custom_button.dart';
 import 'package:shomoshotime/app/modules/custom_bottom_navigation_bar/views/custom_bottom_navigation_bar_view.dart';
 import '../controllers/profile_controller.dart';
@@ -17,7 +18,9 @@ class ProfileView extends GetView<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('ProfileView'), centerTitle: true),
+      appBar: CustomAppBar(
+        title: 'Edit Profile',
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.sp),
@@ -38,11 +41,11 @@ class ProfileView extends GetView<ProfileController> {
                     children: [
                       CircleAvatar(
                         backgroundColor: AppColors.appBarCircleAvatar,
-                        maxRadius: 110,
+                        maxRadius: 80,
                         child: Padding(
-                          padding: EdgeInsets.all(1.sp),
+                          padding: EdgeInsets.all(8.sp),
                           child: InkWell(
-                            onTap: () {},
+                            onTap: (){},
                             child: ClipRRect(
                               borderRadius: BorderRadiusGeometry.circular(100),
                               child: Image.asset(ImagePath.profile, fit: BoxFit.cover),
