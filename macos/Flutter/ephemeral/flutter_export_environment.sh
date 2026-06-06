7ff990c8de968f8bec648590f91ba04f49bd163d
Tuhin Ikbal Eyamin <codereyamin@gmail.com>
2026-06-06 14:31:44 +0600

    local change

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/maktech/fvm/versions/3.41.7"
+export "FLUTTER_APPLICATION_PATH=/Users/maktech/flutter-project/shomoshotime_app_forntend"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.3.1"
+export "FLUTTER_BUILD_NUMBER=10"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
